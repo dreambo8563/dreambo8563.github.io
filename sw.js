2c2f2d8e357f0f9759f1ccae59c4cbb7f6d00c3e
dreambo8563 <dreambo8563@outlook.com>
2020-07-03 23:08:36 +0800

    first blog

--- sw.js
+++ sw.js
@@ -24,15 +24,15 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/2016/05/04/React-Router-Index-Routes-and-Index-Links/');
-
   precacheUrls.push('/2018/12/29/2018年度总结/');
 
-  precacheUrls.push('/2019/01/22/玩耍github/');
+  precacheUrls.push('/2020/01/21/2019总结/');
 
-  precacheUrls.push('/2019/03/29/redis-resp/');
+  precacheUrls.push('/2017/04/27/Actor-Actor-API/');
 
-  precacheUrls.push('/2019/04/23/easy-circular-progress/');
+  precacheUrls.push('/2017/05/11/Actor-FSM/');
+
+  precacheUrls.push('/2017/05/02/Actor-Receive-timeout/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
